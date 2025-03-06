d6ec5d4d56900d4196b8354f33d0556764544e3e
Antonio Augusto Mota Rodrigues <antonio.rodrigues@oi.net.br>
2025-03-06 14:31:31 -0300

    fiz firebase js
     initialize firestore not declared

--- script/services/firebase.js
+++ script/services/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
-import { getFirestore, doc, getDoc, getDocs, collection } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js"
+import { initializeFirestore, collection } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
